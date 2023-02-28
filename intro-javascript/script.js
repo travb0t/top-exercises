114c77bf1f14d62d81cd465e6fa5f3608b834211
travb0t <122580202+travb0t@users.noreply.github.com>
2023-02-28 00:01:28 -0800

    Create style.css, update index and script.js
    Create linked stylesheet that fully centers content of page.
    
    Add follow along arithmetic code to script.js
    
    Add container to display numerical output of script to index.html

--- intro-javascript/script.js
+++ intro-javascript/script.js
@@ -4,8 +4,9 @@ let message = "Yo, ";
 
 let message2 = "... and also Scronk.";
 
-alert(message + hello + message2);
-
-console.log(hello)
+// alert(message + hello + message2);
 
 
+let x = 100 - 31;
+
+document.getElementById("numbers").innerHTML = x;

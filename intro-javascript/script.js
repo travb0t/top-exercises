ee5ab03c0f93be75bd24f2953aac85c6b752b24b
travb0t <122580202+travb0t@users.noreply.github.com>
2023-07-12 16:00:24 -0700

    Syncing with Linux VM
    Adds username entry and check system.

--- intro-javascript/script.js
+++ intro-javascript/script.js
@@ -1,45 +1,43 @@
-let hello = "Waddles wuz h3re";
+// let hello = "Waddles wuz h3re";
 
-let message = "Yo, ";
+// let message = "Yo, ";
 
-let message2 = "... and also Scronk.";
+// let message2 = "... and also Scronk.";
 
 // alert(message + hello + message2);
 
 
 
-let x = 1;
+// let x = 1;
 
-let result = "";
+// let result = "";
 
-while (x != 10000000) {
-    x = x * 10;
-    result = result + x + "<br>";
-}
+// while (x != 10000000) {
+//     x = x * 10;
+//     result = result + x + "<br>";
+// }
 
 // document.getElementById("numbers").innerHTML = result;
 
 
 
-let y = 22 / 7;
+// let y = 22 / 7;
 
-let z = y.toFixed(2);
+// let z = y.toFixed(2);
 
 // document.getElementById("numbers").innerHTML = "Pi =" + " " + z;
 
 
 
-let myNumber = "66";
+// let myNumber = "66";
 
-// myNumber += 3;
-
-myNumber = Number(myNumber) + 3;
+// myNumber = Number(myNumber) + 3;
 
 // document.getElementById("numbers").innerHTML = myNumber;
 
 
 
-let q = (4 + 6 + 9) / 77;
+// let q = (4 + 6 + 9) / 77;
 
 // document.getElementById("numbers").innerHTML = q.toFixed(5);
 
@@ -62,10 +60,40 @@ let q = (4 + 6 + 9) / 77;
 
 
 
-const max = 57;
+// const max = 57;
 
-let actual = max - 13;
+// let actual = max - 13;
 
-let percentage = actual / max;
+// let percentage = actual / max;
 
-document.getElementById("numbers").innerHTML = percentage.toFixed(4);
+// document.getElementById("numbers").innerHTML = percentage.toFixed(4);
+
+
+
+const button = document.querySelector("button");
+
+function greet() {
+    const firstName = prompt("What is your first name?", "");
+    const lastName = prompt("What is your last name?", "");
+    let name;
+
+    if (firstName == 0 || firstName == null) {
+        name = lastName;
+    } else if (lastName == 0 || lastName == null) {
+        name = firstName;
+    } else {
+        name = `${firstName} ${lastName}`;
+    }
+
+    if (name == 0 || name == null) {
+        alert("Please enter at least one name.");
+        greet()
+    } else if (!/^[a-zA-Z\s]+$/.test(name)) { 
+        alert("Please only use letters.");
+        greet()
+    } else {
+        alert(`Hello ${name}, it's nice to meet you!`);
+    }
+}
+
+button.addEventListener("click", greet);
